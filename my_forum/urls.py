ebcd4cc25608ac4b83dfe4035028efacc50f62ed
Waiyaki <muturijames3@gmail.com>
2015-08-23 09:31:51 +0300

    Beginning frontend with AngularJS and Angular Material.

--- my_forum/urls.py
+++ my_forum/urls.py
@@ -28,7 +28,7 @@ router.register(r'comments', views.CommentViewSet)
 
 
 urlpatterns = [
-    url(r'^$', views.api_root),
+    url(r'^api/v1/$', views.api_root),
     url(r'^api/v1/', include(router.urls)),
     url(r'api-auth/', include('rest_framework.urls', namespace="rest_framework")),
 ]
