c603c989845dae2fa83766ec2e244affa5a9da15
Waiyaki <muturijames3@gmail.com>
2015-09-03 21:40:37 +0300

    Disabled debug, added compressor finder for django-compressor

--- my_forum/settings.py
+++ my_forum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u47am8j3-z!m*u(wn@5j_29w9yr)i-9wfh&%xm#5&vj&3(1%@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -130,6 +130,7 @@ STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 
 try:
